46d7698097534d9004a94d6b0ca0063e5c0b7dda
Uksus1147 <tihomirovdima028@gmail.com>
2021-11-30 13:33:02 +0300

    Домашнее задание Тихомирова Дмитрия за 5,6,7 и 8 урок

---
+++ Assets/script/IKforperson.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Animator))]
+public class IKforperson : MonoBehaviour
+{
+    public Animator anim;
+    public bool InvokeIk = false;
+    public Transform head;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (InvokeIk)
+        {
+            anim.SetLookAtPosition(head.position);
+            anim.SetLookAtWeight(1);
+        }
+        else
+        {
+            anim.SetLookAtWeight(0);
+        }
+    }
+}
